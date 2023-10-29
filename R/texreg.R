a8433f9ba819d0b14dd68c0b87a2c4c12598bbff
Nick Ford <105839019+nickforddk@users.noreply.github.com>
2023-10-29 18:42:57 +0100

    Update texreg.R (indent)

--- R/texreg.R
+++ R/texreg.R
@@ -1748,7 +1748,7 @@ matrixreg <- function(l,
     if (output.type[1] == "latex") {
       indentation <- "\\quad "
     } else if (output.type[1] == "html") {
-      indentation <- "<span class='indent'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp</span>;"
+      indentation <- "<span class=\"indent\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>"
     } else {
       indentation <- "    "
     }
